eb203b3d8d31ca80db5dd0ddd6e08efcf6dd0ca8
Thomas Kekeisen <thomas@thomaskekeisen.de>
2015-01-24 11:25:57 +0100

    Added a base contact class

---
+++ Entity/BaseContact.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Mremi\ContactBundle Symfony bundle.
+ *
+ * (c) Rémi Marseille <marseille.remi@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Mremi\ContactBundle\Entity;
+
+use Mremi\ContactBundle\Model\BaseContact as BaseContactModel;
+
+/**
+ * Description of BaseContact
+ *
+ * @author Thomas Kekeisen, http://socialbit.de
+ */
+abstract class BaseContact extends BaseContactModel
+{
+    //put your code here
+}
